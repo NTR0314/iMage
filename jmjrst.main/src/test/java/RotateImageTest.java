dd4e7592b40a0dcab2083a0b1ddc8e2166b1009b
Oswald Zink <oswaldzink@gmail.com>
2018-04-22 23:56:15 +0200

    added null and 0 degree test on rotateImage

--- jmjrst.main/src/test/java/RotateImageTest.java
+++ jmjrst.main/src/test/java/RotateImageTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -5,7 +9,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import org.jis.generator.Generator;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Testclass for method rotateImage() in org.jis.generator.Generator.java
@@ -25,15 +32,40 @@ public class RotateImageTest {
 	 * @throws IOException
 	 *             if invalid path
 	 */
+	@BeforeClass
 	public void getImage() throws IOException {
 		this.image = ImageIO.read(new File(IMAGEPATH));
 	}
 
 	/**
-	 * initialize generator
+	 * Initialize generator
 	 */
 	@Before
 	public void setUp() {
 		this.generator = new Generator(null, 0);
+	}
+	
+	/**
+	 * Reset generator
+	 */
+	@After
+	public void tearDown() {
+		this.generator = null;
+	}
+	
+	/**
+	 * Test if rotating by 0 degrees results in the same image
+	 */
+	@Test
+	public void notRotating() {
+		assertEquals(this.image, this.generator.rotateImage(this.image, 0.0));
+	}
+	
+	/**
+	 * Test if method works with null as image
+	 */
+	@Test
+	public void nullImage() {
+		assertNull(this.generator.rotateImage(null, 0.0));
 	}
 }
