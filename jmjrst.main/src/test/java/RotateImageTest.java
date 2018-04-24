94dda7204b5d2d7254bc7186e6f4a9dbe52ab8b7
Oswald Zink <oswaldzink@gmail.com>
2018-04-24 15:56:23 +0200

    reversed idea, of saving testimages and added 90/270 test

--- jmjrst.main/src/test/java/RotateImageTest.java
+++ jmjrst.main/src/test/java/RotateImageTest.java
@@ -12,6 +12,7 @@ import org.jis.generator.Generator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -25,29 +26,29 @@ public class RotateImageTest {
 	private Generator generator;
 	private BufferedImage image;
 	private static final String IMAGEPATH = "src/test/resources/picture.jpg";
-	private int currentTest = 0;
 
 	/**
 	 * Initialize generator and load picture
-	 * @throws IOException on invalid path
+	 * 
+	 * @throws IOException
+	 *             on invalid path
 	 */
 	@Before
 	public void setUp() throws IOException {
 		this.generator = new Generator(null, 0);
 		this.image = ImageIO.read(new File(IMAGEPATH));
 	}
-	
+
 	/**
 	 * Reset generator
-	 * @throws IOException when saving fails
 	 */
 	@After
-	public void tearDown() throws IOException {
-		ImageIO.write(image, "png", new File("src/test/resources/saved" + Integer.toString(currentTest) + ".png"));
-		currentTest = currentTest + 1;
+	public void tearDown() {
+		// reset
 		this.generator = null;
+		this.image = null;
 	}
-	
+
 	/**
 	 * Test if rotating by 0 degrees results in the same image
 	 */
@@ -55,7 +56,7 @@ public class RotateImageTest {
 	public void notRotating() {
 		assertEquals(this.image, this.generator.rotateImage(this.image, 0.0));
 	}
-	
+
 	/**
 	 * Test if method works with null as image
 	 */
@@ -63,13 +64,37 @@ public class RotateImageTest {
 	public void nullImage() {
 		assertNull(this.generator.rotateImage(null, 0.0));
 	}
-	
+
 	/**
-	 * Testing illegal arguments
+	 * Trivial illegal arguments test
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void argumentTest() {
 		this.generator.rotateImage(this.image, 0.5);
 	}
-	
+
+	/**
+	 * Test if test image rotated by 360 degrees results is the same image, (it
+	 * should be)
+	 */
+	@Test
+	public void rotateThreeSixty() {
+		BufferedImage rotatedImage;
+		
+		rotatedImage = this.generator.rotateImage(this.image, Math.toRadians(90.0)); // rotate by 90 degrees
+		rotatedImage = this.generator.rotateImage(rotatedImage, Math.toRadians(270.0)); // rotate by another
+																									// 270 degrees
+
+		// Test if height and wdith are still the same
+		assertEquals(this.image.getHeight(), rotatedImage.getHeight());
+		assertEquals(this.image.getWidth(), rotatedImage.getWidth());
+
+		// Test if all pixels are the same color as before
+		for (int i = 0; i < image.getWidth(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				assertEquals(image.getRGB(i, j), rotatedImage.getRGB(i, j));
+			}
+		}
+	}
+
 }
