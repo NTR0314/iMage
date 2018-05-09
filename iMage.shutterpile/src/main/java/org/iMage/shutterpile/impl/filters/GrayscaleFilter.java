534e25e6830fb56179295e605ca6b1bd1948aba6
Oswald Zink <oswaldzink@gmail.com>
2018-05-10 01:51:54 +0200

    fixec checkstyle errors

--- iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/filters/GrayscaleFilter.java
+++ iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/filters/GrayscaleFilter.java
@@ -4,43 +4,47 @@ import java.awt.image.BufferedImage;
 
 import org.iMage.shutterpile.port.IFilter;
 
-
 /**
- * This {@link IFilter Filter} converts a picture into a gray picture, by making all rbg values equal.
+ * This {@link IFilter Filter} converts a picture into a gray picture, by making
+ * all rbg values equal.
  * 
  * @author Oswald
  *
  */
 public class GrayscaleFilter implements IFilter {
-	private int averageRGB;
 
 	@Override
 	public BufferedImage apply(BufferedImage arg0) {
 		BufferedImage image = arg0;
-		
+
 		for (int i = 0; i < image.getWidth(); i++) {
 			for (int j = 0; j < image.getHeight(); j++) {
-				image.setRGB(i, j, calcRGB(i,j,image));
+				image.setRGB(i, j, calcRGB(i, j, image));
 			}
 		}
-		
+
 		return image;
 	}
-	
+
 	private int calcRGB(int x, int y, BufferedImage image) {
 		int rgb = image.getRGB(x, y);
 		int alpha = (rgb >> 24) & 0xFF;
-		int red =   (rgb >> 16) & 0xFF;
-		int green = (rgb >>  8) & 0xFF;
-		int blue =  (rgb      ) & 0xFF;
-		
+		int red = (rgb >> 16) & 0xFF;
+		int green = (rgb >> 8) & 0xFF;
+		int blue = rgb & 0xFF;
+
 		int avg = (red + green + blue) / 3;
-		
+
 		int newrgb = avg + (avg << 8) + (avg << 16) + (alpha << 24);
-		
+
 		return newrgb;
 	}
-	
-	public GrayscaleFilter() {}
+
+	/**
+	 * Standard Constructor
+	 */
+	public GrayscaleFilter() {
+
+	}
 
 }
