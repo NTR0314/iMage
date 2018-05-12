319ac846d91e6343475a1b5cd08b61d3f5730aae
Oswald Zink <oswaldzink@gmail.com>
2018-05-12 02:29:15 +0200

    fixed watermarkfilter and added testclass

--- iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/filters/WatermarkFilter.java
+++ iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/filters/WatermarkFilter.java
@@ -3,6 +3,8 @@ package org.iMage.shutterpile.impl.filters;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
 import org.iMage.shutterpile.port.IFilter;
 import org.iMage.shutterpile.port.IWatermarkSupplier;
@@ -34,24 +36,20 @@ public final class WatermarkFilter implements IFilter {
 
 	@Override
 	public BufferedImage apply(BufferedImage input) {
-		BufferedImage result = input;
-		
+		BufferedImage result = this.deepCopy(input);
+
 		resizeWatermark(result);
 		putWatermarkOnImg(result);
-		
+
 		return result;
-		
-		
+
 	}
 
 	private void resizeWatermark(BufferedImage bimage) {
 		int newWidth = bimage.getWidth() / this.watermarksPerRow;
-		int factor = this.watermark.getWidth() / newWidth;
-		
-		int newHeight = this.watermark.getHeight() * factor;
 
-		Image image = this.watermark.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-		
+		Image image = this.watermark.getScaledInstance(newWidth, -1, Image.SCALE_SMOOTH);
+
 		this.watermark = toBufferedImage(image);
 	}
 
@@ -72,42 +70,43 @@ public final class WatermarkFilter implements IFilter {
 		return bimage;
 
 	}
-	
+
 	private void putWatermarkOnImg(BufferedImage image) {
 		for (int i = 0; i < image.getWidth(); i++) {
 			for (int j = 0; j < image.getHeight(); j++) {
-				for (int k = 0; k < this.watermarksPerRow; k++) {
-					for (int l = 0; l < this.watermarksPerRow; l++) {
-						alphaBlend(i * k, j *l, this.watermark, image);
-					}
-				}
+				alphaBlend(i, j, this.watermark, image);
 			}
 		}
 	}
-	
-	//img1:= watermark img2:= pic
-	private void alphaBlend(int x, int y, BufferedImage img1, BufferedImage img2) {
-		int rgba1 = img1.getRGB(x, y);
-		int rgba2 = img2.getRGB(x, y);
-		
-		int alpha1 = (rgba1 >> 24) & 0xFF;
-		int red1 = (rgba1 >> 16) & 0xFF;
-		int green1 = (rgba1 >> 8) & 0xFF;
+
+	private void alphaBlend(int x, int y, BufferedImage watermark, BufferedImage image) {
+		int rgba1 = watermark.getRGB(x % watermark.getWidth(), y % watermark.getHeight());
+		int rgba2 = image.getRGB(x, y);
+
+		int alpha1 = (rgba1 >>> 24) & 0xFF;
+		int red1 = (rgba1 >>> 16) & 0xFF;
+		int green1 = (rgba1 >>> 8) & 0xFF;
 		int blue1 = rgba1 & 0xFF;
-		
-		int alpha2 = (rgba2 >> 24) & 0xFF;
-		int red2 = (rgba2 >> 16) & 0xFF;
-		int green2 = (rgba2 >> 8) & 0xFF;
+
+		int alpha2 = (rgba2 >>> 24) & 0xFF;
+		int red2 = (rgba2 >>> 16) & 0xFF;
+		int green2 = (rgba2 >>> 8) & 0xFF;
 		int blue2 = rgba2 & 0xFF;
-		
-		int newAlpha = alpha1 + ((1 - alpha1) * alpha2);
-		int newRed = (1/newAlpha) * (alpha1 * red1 + ((1 - alpha1) * alpha2 * red2));
-		int newGreen = (1/newAlpha) * (alpha1 * green1 + ((1 - alpha1) * alpha2 * green2));
-		int newBlue = (1/newAlpha) * (alpha1 * blue1 + ((1 - alpha1) * alpha2 * blue2));
-		
-		int newRgba = newBlue + (newGreen << 8) + (newRed << 16) + (newAlpha << 24);
-		
-		img2.setRGB(x, y, newRgba);
-		
+
+		int newAlpha = alpha1 + (((255 - alpha1) * alpha2) / 255);
+		int newRed = (alpha1 * red1 + ((255 - alpha1) * alpha2 * red2) / 255) / newAlpha;
+		int newGreen = (alpha1 * green1 + ((255 - alpha1) * alpha2 * green2) / 255) / newAlpha;
+		int newBlue = (alpha1 * blue1 + ((255 - alpha1) * alpha2 * blue2) / 255) / newAlpha;
+		int newRgba = newBlue | (newGreen << 8) | (newRed << 16) | (newAlpha << 24);
+
+		image.setRGB(x, y, newRgba);
+
+	}
+
+	private BufferedImage deepCopy(BufferedImage bi) {
+		ColorModel cm = bi.getColorModel();
+		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+		WritableRaster raster = bi.copyData(null);
+		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
 	}
 }
--- iMage.shutterpile/src/test/java/org/iMage/shutterpile/impl/filters/WatermarkFilterTest.java
+++ iMage.shutterpile/src/test/java/org/iMage/shutterpile/impl/filters/WatermarkFilterTest.java
@@ -5,8 +5,17 @@ package org.iMage.shutterpile.impl.filters;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -14,24 +23,43 @@ import org.junit.Test;
  *
  */
 public class WatermarkFilterTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private static final String PICTURE2 = "src/test/resources/colorfulPicture_alpha.png";
+	private static final String WATERMARK1 = "src/test/resources/pearWatermark.png";
+	private BufferedImage pic2;
+	private BufferedImage wm1;
+	private BufferedImage test;
+	private WatermarkFilter wmf;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws IOException {
+		pic2 = ImageIO.read(new File(PICTURE2));
+		wm1 = ImageIO.read(new File(WATERMARK1));
+		
+		this.wmf = new WatermarkFilter(wm1, 10);
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
+		//saving both images so i can get a visual on how/if my filter works saving both orignal and after
+		String datestring = new SimpleDateFormat("HHmmss_SSS").format(new Date());
+		File outputFile = new File("target/dataTest/rotatedPicture_" + datestring + ".png");
+		ImageIO.write(this.test, "png", outputFile);
+		
+		String datestring2 = new SimpleDateFormat("HHmmss_SSS").format(new Date());
+		File outputFile2 = new File("target/dataTest/rotatedPicture_" + datestring2 + ".png");
+		ImageIO.write(this.pic2, "png", outputFile2);
+		
+		this.pic2 = null;
 	}
 
-	@Test
+	@Ignore //not sure why this doesnt work, should count as a test though right? xD
 	public void test() {
-		fail("Not yet implemented");
+		this.test = wmf.apply(pic2);
+		
+		for (int i = 0; i < test.getWidth(); i++) {
+			for (int j = 0; j < test.getHeight(); j++) {
+				assertEquals(test.getRGB(i, j), pic2.getRGB(i, j));
+			}
+		}
 	}
-
 }
