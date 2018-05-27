56e0e191dc9280977ef135839ef46cb02ea9efd5
Oswald Zink <oswaldzink@gmail.com>
2018-05-27 14:46:11 +0200

    implemented configure() method in HelloWorldPlugin

--- iMage.facine-plugin/src/main/java/HelloWorldPlugin.java
+++ iMage.facine-plugin/src/main/java/HelloWorldPlugin.java
@@ -1,17 +1,38 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.iMage.plugins.*;
 import org.jis.Main;
 
 public class HelloWorldPlugin extends JmjrstPlugin {
-
+	private final String HOME = System.getProperty("user.home");
+	private final String BILDER = HOME + "/Bilder";
+	private final String PICTURES = HOME + "/Pictures";
+	private final String DESKTOP = HOME + "/Desktop";
+	private final String PICS = HOME + "/pics";
+	
 	@Override
 	public void configure() {
 		JFrame frame = this.setupJFrame();
 		
-		if (condition) {
-			
+		if (Files.exists(Paths.get(BILDER))) {
+			frame.add(new JLabel(BILDER));
 		}
+		
+		if (Files.exists(Paths.get(PICTURES))) {
+			frame.add(new JLabel(PICTURES));
+		}
+		
+		if (Files.exists(Paths.get(DESKTOP))) {
+			frame.add(new JLabel(DESKTOP));
+		}
+		if (Files.exists(Paths.get(PICS))) {
+			frame.add(new JLabel(PICS));
+		}		
+		
 	}
 
 	@Override
@@ -20,14 +41,12 @@ public class HelloWorldPlugin extends JmjrstPlugin {
 	}
 
 	@Override
-	public void init(Main arg0) {
-		String home = System.getProperty("user.home");
-		
+	public void init(Main arg0) {		
 		System.err.println("iMage: Sammelt Ihre Daten seit 2016! Folgende Ordner werden (meist) durchsucht:\n");
-		System.err.println(home + "/Bilder");
-		System.err.println(home + "/Pictures");
-		System.err.println(home + "/Desktop");
-		System.err.println(home + "/pics");
+		System.err.println(BILDER);
+		System.err.println(PICTURES);
+		System.err.println(DESKTOP);
+		System.err.println(PICS);
 	}
 
 	@Override
