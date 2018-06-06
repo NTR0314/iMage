84526c4752d452991bc06b45a0bedcb598ebc2b9
Oswald Zink <oswaldzink@gmail.com>
2018-06-06 19:40:25 +0200

    added checkstyle and src xml also created mainclass for window

---
+++ iMage.iDeal/src/main/java/iMage/iDeal/MainWindow.java
@@ -0,0 +1,19 @@
+package iMage.iDeal;
+
+import javax.swing.JFrame;
+
+/**
+ * This class is the main window for the GUI of iMage
+ * @author Oswald
+ *
+ */
+public class MainWindow extends JFrame {
+	private static final long serialVersionUID = 4424146395462393900L;
+	
+	private void setUpWindow() {
+		//TODO IMPLEMENT
+	}
+	
+
+	
+}
