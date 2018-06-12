22fad0d1a653adfdb7bdbfb304b8aedd5f2e23eb
Oswald Zink <oswaldzink@gmail.com>
2018-06-12 02:54:11 +0200

    need to push on laptop so i can continue on PC

--- iMage.iDeal/src/main/java/iMage/iDeal/MainWindow.java
+++ iMage.iDeal/src/main/java/iMage/iDeal/MainWindow.java
@@ -2,8 +2,8 @@ package iMage.iDeal;
 
 import javax.swing.*;
 import java.awt.*;
-
-//TODO add dependancy to shutterpile
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * This class is the main window for the GUI of iMage
@@ -16,12 +16,19 @@ public class MainWindow extends JFrame {
     private static final String OUTPUT_PATH = "src/resources/Output.png";
     private static final String WATERMARK_PATH = "src/resources/Watermark.png";
 
-    private void setUpJButton() {
+    private JButton createInputJButton() throws IOException {
+        BufferedImage inputImage = Utils.getImage("Input", 200, 150);
+        JButton inputButton = new JButton(new ImageIcon(inputImage));
+        inputButton.setBorder(BorderFactory.createEmptyBorder());
+        inputButton.setContentAreaFilled(false);
+        inputButton.setVisible(true);
 
+        return inputButton;
     }
 
     /**
      * Main method of the GUI: sets up the main window
+     *
      * @param args arguements
      */
     public static void main(String[] args) {
@@ -35,9 +42,21 @@ public class MainWindow extends JFrame {
         this.setResizable(false);
         this.setTitle("iDeal");
         this.setLayout(new BorderLayout());
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        try {
+            topPanel.add(createInputJButton());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.add(topPanel);
+
+
         this.setVisible(true);
 
     }
+
 
 
 }
