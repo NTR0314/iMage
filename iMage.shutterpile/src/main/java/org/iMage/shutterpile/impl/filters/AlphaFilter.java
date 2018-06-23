10b878b4e3ea2b40aa02f382ed8c2b8f7448c586
Oswald Zink <oswaldzink@gmail.com>
2018-06-23 15:45:01 +0200

    created & implemented AlphaFilter with desired function

---
+++ iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/filters/AlphaFilter.java
@@ -0,0 +1,27 @@
+package org.iMage.shutterpile.impl.filters;
+
+import org.iMage.shutterpile.impl.supplier.ImageWatermarkSupplier;
+import org.iMage.shutterpile.port.IFilter;
+
+import java.awt.image.BufferedImage;
+
+public class AlphaFilter implements IFilter {
+    @Override
+    public BufferedImage apply(BufferedImage bufferedImage) {
+        BufferedImage result = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        for (int i = 0; i < result.getWidth(); i++) {
+            for (int j = 0; j < result.getHeight(); j++) {
+                int color = bufferedImage.getRGB(i, j);
+                int alpha = color >> 24 & 0x000000FF;
+                alpha = (alpha * ImageWatermarkSupplier.DEFAULT_FACTOR) / 100;
+                result.setRGB(i, j, (color & 0x00FFFFFF) | (alpha << 24));
+            }
+        }
+        result.flush();
+
+        return result;
+    }
+
+
+}
